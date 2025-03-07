d8d4c779a9bdd96a4001287950d6dde6e0a66c2a
Marglodis Tovar <marglodistovar@gmail.com>
2025-03-07 16:15:01 -0300

    Configuración de rutas

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   imports: [],
   templateUrl: './change-detection.component.html',
   styles: ``
 })
-export class ChangeDetectionComponent {
+export default class ChangeDetectionComponent {
 
 }

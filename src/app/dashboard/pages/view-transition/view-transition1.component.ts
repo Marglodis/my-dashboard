4116ed90fd4e410f2b73051cc787519f8f5311fe
Marglodis Tovar <marglodistovar@gmail.com>
2025-03-15 11:48:46 -0300

    Hero animation - transition

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -8,9 +8,11 @@ import { TitleComponent } from '@shared/title/title.component';
     <app-title title="View Transition 1"></app-title>
 
     <section class="flex justify-start">
-      <img src="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300">
+      <img src="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"
+      style="view-transition-name: hero1">
 
-      <div class="bg-blue-500 w-56 h-56">
+      <div class="bg-blue-500 w-56 h-56"
+      style="view-transition-name: hero2">
 
       </div>
     </section>
--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -7,10 +7,12 @@ import { TitleComponent } from '@shared/title/title.component';
   template: `
     <app-title title="View Transition 2"></app-title>
 
-    <section class="flex justify-start">
-      <img src="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300">
+    <section class="flex justify-end">
+      <img src="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"
+      style="view-transition-name: hero1">
 
-      <div class="bg-blue-500 w-56 h-56">
+      <div class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"
+      style="view-transition-name: hero2">
 
       </div>
     </section>
